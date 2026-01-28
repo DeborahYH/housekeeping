275ae5675d84f849acf7b3692e21b61437927f8d
Deborah Y Hukuda <deborah.hukuda@gmail.com>
2026-01-28 16:16:40 -0300

    Adicao do perfil e das abas na página do usuário

---
+++ users/static/users/js/tabs.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.tab-content');
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+
+        buttons.forEach(btn => btn.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+
+        button.classList.add('active');
+        contents[index].classList.add('active');
+    });
+});
